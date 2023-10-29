6cdb52d8e132fd8cc846f4ff220dc8840e91adf3
Ramon <iramonsx@gmail.com>
2023-10-29 18:44:22 -0300

    Initial Commit

---
+++ iResidential/iResidentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iResidentialApp.swift
+//  iResidential
+//
+//  Created by Ramon Santos on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iResidentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
